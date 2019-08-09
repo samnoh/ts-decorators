a01c625125e28a99a36507838bbcde34e92ef344
Sam Noh <samnoh21@outlook.com>
2019-08-09 22:09:36 +1200

    mod - index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-@classDecorator
 class User {
     constructor(public name: string) {}
 
@@ -7,12 +6,28 @@ class User {
     }
 }
 
-function classDecorator(constructor: Function) {
-    console.log(constructor);
-}
-
 function parameterDecorator(target: User, key: string, index: number): void {
     console.log(key, index);
 }
 
 new User('smith').printName('Hello');
+
+//
+
+@classDecorator
+class Greeter {
+    property = 'property';
+    hello: string;
+    constructor(m: string) {
+        this.hello = m;
+    }
+}
+
+function classDecorator<T extends { new (...args: any[]): {} }>(constructor: T) {
+    return class extends constructor {
+        newProperty = 'new property';
+        hello = 'override';
+    };
+}
+
+console.log(new Greeter('world'));
